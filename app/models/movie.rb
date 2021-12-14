8433cfb8e1f358221532e980455a77313819acd0
Devin Pile <devinpile@gmail.com>
2021-12-14 01:14:11 -0700

    Add changes to implement story2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,12 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :movie_actors
   has_many :actors, through: :movie_actors
+
+  def average_age
+    actors.average(:age)
+  end
+
+  def order_actors
+    actors.order(:age)
+  end 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
-universal = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+# universal = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+#
+# universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
+# universal.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy')
 
-universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
-universal.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy')
+@studio = Studio.create!(name: "Syntax Studios", location: "Burbank")
+@movie = @studio.movies.create!(title: "Active Record", creation_year: 2022, genre: "Suspense/Thriller")
+@actor1 = Actor.create!(name: "Ruby Rails", age: 26)
+@actor2 = Actor.create!(name: "Mercy Quill", age: 30)
+@actor3 = Actor.create!(name: "Repo Pomodoro", age: 22)
+@movie_actor = MovieActor.create!(movie_id: @movie.id, actor_id:@actor1.id)
+@movie_actor = MovieActor.create!(movie_id: @movie.id, actor_id:@actor2.id)
+@movie_actor = MovieActor.create!(movie_id: @movie.id, actor_id:@actor3.id)
